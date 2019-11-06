5c1ec81c3fbcea978e7ddc6bd200d3befe95b746
Lee <416606903@outlook.com>
2019-11-06 02:52:18 +0000

    商品详情表字段补全

---
+++ database/migrations/2019_11_05_154643_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id')->nullable()->comment('类目ID，级联清空,允许null');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->string('title')->comment('商品标题');
+            $table->text('description')->comment('商品详情');
+            $table->text('attrbutes')->comment('商品参数');
+            $table->string('brand')->comment('商品品牌');
+            $table->boolean('type')->comment('商品新旧类型');
+            $table->boolean('on_sale')->default(true)->comment("是否上架，默认上架");
+            $table->string('images')->comment('商品图片路径');
+            $table->string("optinons")->nullable()->comment('增值服务');
+            $table->decimal('price',10,2)->default(0)->comment('商品价格');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
