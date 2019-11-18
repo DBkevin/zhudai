d886c9f91ce7d9f447a0924eaaeb3b736c763b76
Lee <416606903@outlook.com>
2019-11-18 10:01:19 +0000

    关于我们等页面模型

---
+++ database/migrations/2019_11_18_174452_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->comment('标题');
+            $table->string('type')->nullable()->comment('所属类目');
+            $table->text('body')->comment('正文');
+            $table->boolean('on_type')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
